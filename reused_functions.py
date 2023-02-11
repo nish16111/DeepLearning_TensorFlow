176098ddbb6f0a30ad360dfe3e8efb9da0553beb
Nishant Sirwani <85132624+nish16111@users.noreply.github.com>
2023-02-11 13:36:08 +0530

    Update reused_functions.py

--- reused_functions.py
+++ reused_functions.py
@@ -205,9 +205,10 @@ def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_s
 
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 
+# Function to calculate accuracy, precision, recall and f1-score.
 def calculate_results(y_true, y_pred):
   """
-  Calculates model accuracy, precision, recall and f1 score of a binary classification model.
+  Calculates model accuracy, precision, recall and f1 score.
   Args:
       y_true: true labels in the form of a 1D array
       y_pred: predicted labels in the form of a 1D array
@@ -267,25 +268,11 @@ def compare_historys(original_history, new_history, initial_epochs=5):
     plt.xlabel('epoch')
     plt.show()
     
-# Function to evaluate accuracy, precision, recall and f1-score.
-from sklearn.metrics import accuracy_score, precision_recall_fscore_support    
-def calculate_results(y_true, y_pred):
-  """
-  Calculates model accuracy, precision, recall and f1 score.
 
-  Args:
-      y_true: true labels in the form of 1D array
-      y_pred: predicted labels in the form of 1D array
 
-  Returns a dictionary of accuracy, precision, recall, f1-score.
-  """
 
-  model_accuracy = accuracy_score(y_true, y_pred) * 100
-  precision, recall, f1_score, _ = precision_recall_fscore_support(y_true, y_pred, average = "weighted")
 
-  model_results = {"accuracy": model_accuracy,
-                   "precision": precision,
-                   "recall": recall,
-                   "f1_score": f1_score}
 
-  return model_results
+
+
+
